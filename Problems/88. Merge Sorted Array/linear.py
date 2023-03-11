31680237d7c1d16e3498d6a4718941f0fcc2af70
Shakib <40121837+Shakkak@users.noreply.github.com>
2023-03-11 20:28:52 +0330

    Update linear.py

--- Problems/88. Merge Sorted Array/linear.py
+++ Problems/88. Merge Sorted Array/linear.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        while (n > 0 ):
+        while (n > 0):
             if m <= 0 or nums2[n-1]>= nums1[m-1]:
                 nums1[m+n-1] = nums2[n-1]
                 n -= 1
